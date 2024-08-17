235f24b80b5d31c1009019e614b2b07210f8c24c
jimmyvallejo <jrv1295@gmail.com>
2024-08-17 16:25:41 -0400

    Updates

--- src/constants/personal_projects.js
+++ src/constants/personal_projects.js
@@ -3,14 +3,14 @@ export const p3Name = "Breakout";
 export const p4Name = "Thinkbot";
 export const p5Name = "ChatMD";
 
-export const p1 = `Full-stack app using React, Express, and MongoDB. It's a mix of Twitter, Craigslist, and Yelp, with weather, Stripe, and Google Maps API for payments and location features. Share information about local businesses, traffic, or entertainment, post classified ads, or connect with others.`;
-export const p3 = `Breakout, a classic arcade game remade in JavaScript canvas with clean and reusable code using class-based programming. Experience an engaging and challenging gameplay loop with advanced collision detection and ball physics.`;
+export const p1 = `Social media application. A mix of Twitter, Craigslist, and Yelp. It features weather, Stripe, and Google Maps API integrations for payments and location services. Share information about local businesses, traffic, or entertainment, post classified ads, or connect with others.`;
+export const p3 = `Breakout, a classic arcade game remade in JavaScript canvas using object oriented game design principles. Experience an engaging and challenging gameplay loop with advanced collision detection and ball physics.`;
 export const p4 = `AI tutor created for the 2023 Emerge Americas hackathon. Students get personalized responses from the AI based on subject and age. Teachers get access to dynamic student data.`;
-export const p5 = `ChatMD is an AI replacement for WebMD. Get access to responses tailored to your pre-existing conditions, search for local medical providers, save your chats for later, and more.`;
+export const p5 = `ChatMD is an AI replacement for WebMD. Get access to responses tailored to your pre-existing conditions, search for local medical providers, save your chats for later, and more. Integrated with ChatGPT and health.gov API of over 10,000 medical conditions.`;
 
-export const p1Tech = "React | Express | MongoDB | Node.js";
+export const p1Tech = "React | Node.js | MongoDB | Stripe";
 export const p3Tech = "JavaScript | Canvas | HTML | CSS";
-export const p4Tech = "React | Express | MongoDB | OpenAI";
+export const p4Tech = "React | Node.js | MongoDB | OpenAI";
 
 export const p1Img = process.env.PUBLIC_URL + "tipster.jpg";
 export const p3Img = process.env.PUBLIC_URL + "brick.jpg";
